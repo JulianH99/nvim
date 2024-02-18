825a6154d3296deb498cf61c7bc28e0ac4b15121
JulianH99 <juliancorredor99@gmail.com>
2024-02-18 12:08:34 -0500

    updated indent on mason

--- lua/bat99/plugins/lsp/mason.lua
+++ lua/bat99/plugins/lsp/mason.lua
@@ -1,169 +1,169 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"folke/neodev.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local masonlsp = require("mason-lspconfig")
-		local lsp = require("lspconfig")
-		local cmplsp = require("cmp_nvim_lsp")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "folke/neodev.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local masonlsp = require("mason-lspconfig")
+    local lsp = require("lspconfig")
+    local cmplsp = require("cmp_nvim_lsp")
 
-		local neodev = require("neodev")
+    local neodev = require("neodev")
 
-		-- TODO: set all telescope pickers with the ivy theme
+    -- TODO: set all telescope pickers with the ivy theme
 
-		local keymap = vim.keymap -- for conciseness
+    local keymap = vim.keymap -- for conciseness
 
-		local opts = { noremap = true, silent = true }
-		local on_attach = function(_, bufnr)
-			opts.buffer = bufnr
+    local opts = { noremap = true, silent = true }
+    local on_attach = function(_, bufnr)
+      opts.buffer = bufnr
 
-			-- set keybinds
-			opts.desc = "Show LSP references"
-			keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+      -- set keybinds
+      opts.desc = "Show LSP references"
+      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-			opts.desc = "Go to declaration"
-			keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+      opts.desc = "Go to declaration"
+      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-			opts.desc = "Show LSP definitions"
-			keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+      opts.desc = "Show LSP definitions"
+      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-			opts.desc = "Show LSP implementations"
-			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+      opts.desc = "Show LSP implementations"
+      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-			opts.desc = "Show LSP type definitions"
-			keymap.set("n", "gT", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+      opts.desc = "Show LSP type definitions"
+      keymap.set("n", "gT", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-			opts.desc = "See available code actions"
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+      opts.desc = "See available code actions"
+      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-			opts.desc = "Smart rename"
-			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+      opts.desc = "Smart rename"
+      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+      opts.desc = "Show buffer diagnostics"
+      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-			opts.desc = "Show line diagnostics"
-			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+      opts.desc = "Show line diagnostics"
+      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-			opts.desc = "Go to previous diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+      opts.desc = "Go to previous diagnostic"
+      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-			opts.desc = "Go to next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+      opts.desc = "Go to next diagnostic"
+      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-			opts.desc = "Show list of diagnostics"
-			keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+      opts.desc = "Show list of diagnostics"
+      keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
-			opts.desc = "Show documentation for what is under cursor"
-			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+      opts.desc = "Show documentation for what is under cursor"
+      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-			opts.desc = "Restart LSP"
-			keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-		end
+      opts.desc = "Restart LSP"
+      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+    end
 
-		-- setup capabilities
-		local capabilities = cmplsp.default_capabilities()
+    -- setup capabilities
+    local capabilities = cmplsp.default_capabilities()
 
-		mason.setup()
+    mason.setup()
 
-		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		-- for type, icon in pairs(signs) do
-		-- 	local hl = "DiagnosticSign" .. type
-		-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		-- end
-		--
-		local html_file_types = {
-			"html",
-			"typescriptreact",
-			"javascriptreact",
-			"css",
-			"sass",
-			"scss",
-			"less",
-			"svelte",
-			"liquid",
-			"templ",
-			"astro",
-			"htmldjango",
-			"phpactor",
-		}
+    -- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    -- for type, icon in pairs(signs) do
+    -- 	local hl = "DiagnosticSign" .. type
+    -- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    -- end
+    --
+    local html_file_types = {
+      "html",
+      "typescriptreact",
+      "javascriptreact",
+      "css",
+      "sass",
+      "scss",
+      "less",
+      "svelte",
+      "liquid",
+      "templ",
+      "astro",
+      "htmldjango",
+      "phpactor",
+    }
 
-		neodev.setup({})
-		masonlsp.setup({
-			ensure_installed = {
-				"tsserver",
-				"html",
-				"cssls",
-				"lua_ls",
-				"emmet_ls",
-				"gopls",
-				"theme_check",
-				"templ",
-				"astro",
-				"pyright",
-				"tailwindcss",
-				"jsonls",
-			},
+    neodev.setup({})
+    masonlsp.setup({
+      ensure_installed = {
+        "tsserver",
+        "html",
+        "cssls",
+        "lua_ls",
+        "emmet_ls",
+        "gopls",
+        "theme_check",
+        "templ",
+        "astro",
+        "pyright",
+        "tailwindcss",
+        "jsonls",
+      },
 
-			automatic_installation = true,
-			handlers = {
-				function(server_name) -- default handler (optional)
-					lsp[server_name].setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["html"] = function()
-					lsp.html.setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-						filetypes = html_file_types,
-					})
-				end,
-				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({
-						capabilities = capabilities,
-						on_attach = on_attach,
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = { "vim", "awesome", "screen", "client", "tag" },
-								},
-							},
-						},
-					})
-				end,
-				["emmet_ls"] = function()
-					lsp["emmet_ls"].setup({
-						capabilities = capabilities,
-						on_attach = on_attach,
-						filetypes = html_file_types,
-					})
-				end,
-				["templ"] = function()
-					lsp.templ.setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-						filetypes = { "templ" },
-					})
-				end,
-				["tailwindcss"] = function()
-					lsp.tailwindcss.setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-						filetypes = html_file_types,
-					})
-				end,
-			},
-		})
+      automatic_installation = true,
+      handlers = {
+        function(server_name) -- default handler (optional)
+          lsp[server_name].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["html"] = function()
+          lsp.html.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            filetypes = html_file_types,
+          })
+        end,
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim", "awesome", "screen", "client", "tag" },
+                },
+              },
+            },
+          })
+        end,
+        ["emmet_ls"] = function()
+          lsp["emmet_ls"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            filetypes = html_file_types,
+          })
+        end,
+        ["templ"] = function()
+          lsp.templ.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            filetypes = { "templ" },
+          })
+        end,
+        ["tailwindcss"] = function()
+          lsp.tailwindcss.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            filetypes = html_file_types,
+          })
+        end,
+      },
+    })
 
-		vim.filetype.add({ extension = { templ = "templ" } })
-		-- set diagnostic keymab
-	end,
+    vim.filetype.add({ extension = { templ = "templ" } })
+    -- set diagnostic keymab
+  end,
 }
