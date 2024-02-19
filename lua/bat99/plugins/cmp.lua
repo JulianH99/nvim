6c7a11823b5d33d253a4e9800f204b97c21eab1c
JulianH99 <juliancorredor99@gmail.com>
2024-02-18 20:29:30 -0500

    Cmp for cmdline and telescope goodies

--- lua/bat99/plugins/cmp.lua
+++ lua/bat99/plugins/cmp.lua
@@ -1,66 +1,83 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
-		-- snippets
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		-- lsp kind
-		"onsails/lspkind.nvim",
-	},
-	config = function()
-		local cmp = require("cmp")
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    -- snippets
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    -- lsp kind
+    "onsails/lspkind.nvim",
+  },
+  config = function()
+    local cmp = require("cmp")
 
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
 
-		require("luasnip.loaders.from_lua").lazy_load({ paths = { "./snippets" } })
+    require("luasnip.loaders.from_lua").lazy_load({ paths = { "./snippets" } })
 
-		cmp.setup({
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol",
-					maxwidth = 80,
-					ellipisis_char = "...",
-				}),
-			},
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-2),
-				["<C-f>"] = cmp.mapping.scroll_docs(2),
-				["<C-space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<c-n>"] = cmp.mapping(function()
-					luasnip.jump(1)
-				end, { "i", "s" }),
-				["<c-p>"] = cmp.mapping(function()
-					luasnip.jump(-1)
-				end, { "i", "s" }),
-			}),
-			-- configure sources
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-			}),
-			experimental = {
-				ghost_text = true,
-			},
-		})
-	end,
+    cmp.setup({
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol",
+          maxwidth = 80,
+          ellipisis_char = "...",
+        }),
+      },
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-2),
+        ["<C-f>"] = cmp.mapping.scroll_docs(2),
+        ["<C-space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<c-n>"] = cmp.mapping(function()
+          luasnip.jump(1)
+        end, { "i", "s" }),
+        ["<c-p>"] = cmp.mapping(function()
+          luasnip.jump(-1)
+        end, { "i", "s" }),
+      }),
+      -- configure sources
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+      experimental = {
+        ghost_text = true,
+      },
+    })
+
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+  end,
 }
--- lua/bat99/plugins/telescope.lua
+++ lua/bat99/plugins/telescope.lua
@@ -1,74 +1,74 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	event = "VeryLazy",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	config = function()
-		local telescope = require("telescope")
-		local builtin = require("telescope.builtin")
-		local actions = require("telescope.actions")
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+    local actions = require("telescope.actions")
 
-		telescope.load_extension("ui-select")
+    telescope.load_extension("ui-select")
 
-		vim.keymap.set("n", "<leader>ff", function()
-			builtin.find_files({
-				find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
-				previewer = false,
-			})
-		end, {})
-		vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fS", builtin.grep_string, { silent = true })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-		vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, {})
-		vim.keymap.set("n", "<leader>fr", builtin.git_files, { noremap = true, silent = true })
-		vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+    vim.keymap.set("n", "<leader>ff", function()
+      builtin.find_files({
+        find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
+        previewer = false,
+      })
+    end, {})
+    vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>fS", builtin.grep_string, { silent = true })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, {})
+    vim.keymap.set("n", "<leader>fr", builtin.git_files, { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
 
-		telescope.setup({
-			theme = "evil",
+    telescope.setup({
+      theme = "ivy",
 
-			pickers = {
-				live_grep = {
-					theme = "ivy",
-				},
-				find_files = {
-					theme = "ivy",
-					preview = true,
-				},
-				buffers = {
-					theme = "ivy",
-				},
-				current_buffer_fuzzy_find = {
-					theme = "ivy",
-				},
-				git_branches = {
-					theme = "ivy",
-				},
-				git_files = {
-					theme = "ivy",
-					show_untracked = true,
-				},
-				lsp_references = {
-					theme = "ivy",
-				},
-			},
-			defaults = {
-				preview = false,
-				mappings = {
-					n = {
-						["<c-d>"] = actions.delete_buffer,
-					},
-					i = {
-						["<c-d>"] = actions.delete_buffer,
-					},
-				},
-			},
-		})
+      pickers = {
+        live_grep = {
+          theme = "ivy",
+        },
+        find_files = {
+          theme = "ivy",
+        },
+        buffers = {
+          theme = "ivy",
+        },
+        current_buffer_fuzzy_find = {
+          theme = "ivy",
+        },
+        git_branches = {
+          theme = "ivy",
+        },
+        git_files = {
+          theme = "ivy",
+          show_untracked = true,
+        },
+        lsp_references = {
+          theme = "ivy",
+        },
+      },
+      defaults = {
+        theme = "ivy",
+        previewer = true,
+        mappings = {
+          n = {
+            ["<c-d>"] = actions.delete_buffer,
+          },
+          i = {
+            ["<c-d>"] = actions.delete_buffer,
+          },
+        },
+      },
+    })
 
-		telescope.load_extension("fzf")
-	end,
+    telescope.load_extension("fzf")
+  end,
 }
